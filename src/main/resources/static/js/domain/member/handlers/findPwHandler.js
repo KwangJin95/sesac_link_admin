a6b9483e5e8844504649352857f9ce61e82dd588
KwangJin95 <rhkdwls95@naver.com>
2025-06-17 16:27:43 +0900

    Update findPwHandler.js

--- src/main/resources/static/js/domain/member/handlers/findPwHandler.js
+++ src/main/resources/static/js/domain/member/handlers/findPwHandler.js
@@ -173,6 +173,8 @@ export async function onEmailCodeSendBtnClick(event, time) {
     // 인증 코드 전송 및 받아옴
     sendEmailVerifyCode(emailInput.value.trim())
         .then(data => {
+            // 수정 필요
+            alert(data);
             setEmailCode(data);
         })
         .catch(e => console.log(e));
